cc0cab29348d4dacb076a734545cba8f883db6e5
Penguin <say8425@gmail.com>
2017-01-09 19:03:03 +0900

    apply pagination customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,7 @@ class CustomersController < ApplicationController
   def index
     if params[:keywords].present?
       @keywords = params[:keywords]
-      @customers = Customer.search_keyword(@keywords)
+      @customers = Customer.search_keyword(@keywords).page(params[:page]).per(10)
     else
       @customers = []
     end
